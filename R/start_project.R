f394fc6f4aca00d98851e2d9c292a1ed487fd811
Michael Lopez <michael@childpovertyactionlab.org>
2023-04-16 19:25:32 -0500

    Update start_project.R

--- R/start_project.R
+++ R/start_project.R
@@ -14,7 +14,13 @@
 
 start_project <- function(name, directory = getwd(), readme = TRUE, gitignore = TRUE, shiny = FALSE) {
 
-  usethis::create_project(paste0(directory, "/", name))
+  if (shiny == TRUE) {
+    usethis::use_directory(path = paste0(directory, "/", name, "/Shiny"))
+  }
+
+  usethis::use_directory(path = paste0(directory, "/", name, "/Data"))
+  usethis::use_directory(path = paste0(directory, "/", name, "/Scripts"))
+  usethis::use_directory(path = paste0(directory, "/", name, "/Graphics"))
 
   if (readme == TRUE) {
     use_readme_cpal(name = name, open = FALSE)
@@ -24,12 +30,6 @@ start_project <- function(name, directory = getwd(), readme = TRUE, gitignore = 
     use_git_ignore_cpal(gitignore = "R", open = FALSE)
   }
 
-  if (shiny == TRUE) {
-    usethis::use_directory(path = paste0(directory, "/", name, "/Shiny"))
-  }
-
-  usethis::use_directory(path = paste0(directory, "/", name, "/Data"))
-  usethis::use_directory(path = paste0(directory, "/", name, "/Scripts"))
-  usethis::use_directory(path = paste0(directory, "/", name, "/Graphics"))
+  usethis::create_project(paste0(directory, "/", name))
 
 }
