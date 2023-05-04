90ca370bb74149c66610ee9c04cb08f6505f9c9f
Michael Lopez <michael@childpovertyactionlab.org>
2023-05-04 13:14:41 -0500

    image and template changes

--- R/use_content.R
+++ R/use_content.R
@@ -22,12 +22,14 @@ use_content <- function(content) {
     img_logo_teal <- magick::image_read(system.file("content/CPAL_Logo_Teal.png", package = "cpaltemplates"))
     img_logo_white <- magick::image_read(system.file("content/CPAL_Logo_White.png", package = "cpaltemplates"))
     img_favi <- magick::image_read(system.file("content/CPAL_favicon.ico", package = "cpaltemplates"))
-    img_sky <- magick::image_read(system.file("content/CPAL_Skyline_Name_White.png", package = "cpaltemplates"))
+    img_skywh <- magick::image_read(system.file("content/CPAL_Skyline_White.png", package = "cpaltemplates"))
+    img_skyte <- magick::image_read(system.file("content/CPAL_Skyline_Teal.png", package = "cpaltemplates"))
 
     magick::image_write(img_logo_teal, "www/images/CPAL_Logo_White.png")
     magick::image_write(img_logo_white, "www/images/CPAL_Logo_Teal.png")
     magick::image_write(img_favi, "www/images/CPAL_favicon.ico")
-    magick::image_write(img_sky, "www/images/CPAL_Skyline_Name_White.png")
+    magick::image_write(img_skywh, "www/images/CPAL_Skyline_White.png")
+    magick::image_write(img_skyte, "www/images/CPAL_Skyline_Teal.png")
 
   } else {
 
