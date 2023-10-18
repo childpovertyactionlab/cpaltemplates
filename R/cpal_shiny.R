ffdf5136ff50d694f0a3fa393169ba6ff653b658
Michael Lopez <68875922+lopezmichael@users.noreply.github.com>
2023-10-17 23:40:22 -0500

    style updates

--- R/cpal_shiny.R
+++ R/cpal_shiny.R
@@ -7,24 +7,24 @@
 cpal_shiny <- function(){
   bslib::bs_theme(
     version = 5,
-    bg = "#E7ECEE",
+    bg = "#FFFFFF",
     fg = "#008097",
-    primary = "#008097",
-    secondary = "#E98816",
+    primary = "#042D33",
+    secondary = "#881354",
     success = "#009763",
-    info = "#008097",
-    warning = "#E98816",
-    danger = "#971700",
+    info = "#FEF6C7",
+    warning = "#ED683F",
+    danger = "#881354",
     font_scale = NULL,
     heading_font = bslib::font_google("Poppins"),
     base_font = bslib::font_google("Roboto"),
     code_font = bslib::font_google("Fira Code"),
     "enable-gradients" = TRUE,
     "enable-shadows" = TRUE,
-    "progress-bar-bg" = "#E98816", # progress bar color
-    "progress-bg" = "#595959", # progress bar background color
-    "body-color" = "#3f3f3f", # body text color
+    "progress-bar-bg" = "#042D33", # progress bar color
+    "progress-bg" = "#83b6bf", # progress bar background color
+    "body-color" = "#2F2F2F", # body text color
 
     bootswatch = "sandstone" # base shiny theme to modify
   )
-  }
+}
