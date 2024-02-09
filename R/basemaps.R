1e4334e47cf5ac3b7d56319bd808b11b3ebc4916
Michael Lopez <68875922+lopezmichael@users.noreply.github.com>
2024-02-09 11:28:29 -0600

    Update basemaps.R

--- R/basemaps.R
+++ R/basemaps.R
@@ -7,8 +7,8 @@
 #' @family cpal_mapbox
 #' @rdname cpal_mapbox
 #' @export
-cpal_mapbox_color <- "https://api.mapbox.com/styles/v1/michaelcpal/clqzkwbw800sw01nv90032l7z/wmts?access_token=pk.eyJ1IjoibWljaGFlbGNwYWwiLCJhIjoiY2tnMmI5ejgyMDFhZDJxdDNqZjV4cmJ1aiJ9.Wy-HsM9Q6hyabrmIezakbQ"
+cpal_mapbox_color <- "https://api.mapbox.com/styles/v1/michaelcpal/clqzkwbw800sw01nv90032l7z/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWljaGFlbGNwYWwiLCJhIjoiY2tnMmI5ejgyMDFhZDJxdDNqZjV4cmJ1aiJ9.Wy-HsM9Q6hyabrmIezakbQ"
 
 #' @rdname cpal_mapbox
 #' @export
-cpal_mapbox_mono <- "https://api.mapbox.com/styles/v1/michaelcpal/clsevmo9903ac01qq15lzg6r4/wmts?access_token=pk.eyJ1IjoibWljaGFlbGNwYWwiLCJhIjoiY2tnMmI5ejgyMDFhZDJxdDNqZjV4cmJ1aiJ9.Wy-HsM9Q6hyabrmIezakbQ"
+cpal_mapbox_mono <- "https://api.mapbox.com/styles/v1/michaelcpal/clsevmo9903ac01qq15lzg6r4/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWljaGFlbGNwYWwiLCJhIjoiY2tnMmI5ejgyMDFhZDJxdDNqZjV4cmJ1aiJ9.Wy-HsM9Q6hyabrmIezakbQ"
