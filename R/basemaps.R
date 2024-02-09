e22dbfa8b38eeea150f65028b15897ecd9150946
Michael Lopez <68875922+lopezmichael@users.noreply.github.com>
2024-02-09 11:08:33 -0600

    mapbox fixes

---
+++ R/basemaps.R
@@ -0,0 +1,14 @@
+#' Child Poverty Action Lab Mapbox Basemaps
+#'
+#' @source Child Poverty Action Lab Data Visualization Style Guide
+#'  \url{http://childpovertyactionlab.github.io/cpal-directory/}
+#'
+#'
+#' @family cpal_mapbox
+#' @rdname cpal_mapbox
+#' @export
+cpal_mapbox_color <- "https://api.mapbox.com/styles/v1/michaelcpal/clqzkwbw800sw01nv90032l7z/wmts?access_token=pk.eyJ1IjoibWljaGFlbGNwYWwiLCJhIjoiY2tnMmI5ejgyMDFhZDJxdDNqZjV4cmJ1aiJ9.Wy-HsM9Q6hyabrmIezakbQ"
+
+#' @rdname cpal_mapbox
+#' @export
+cpal_mapbox_mono <- "https://api.mapbox.com/styles/v1/michaelcpal/clsevmo9903ac01qq15lzg6r4/wmts?access_token=pk.eyJ1IjoibWljaGFlbGNwYWwiLCJhIjoiY2tnMmI5ejgyMDFhZDJxdDNqZjV4cmJ1aiJ9.Wy-HsM9Q6hyabrmIezakbQ"
--- R/cpal_mapbox.R
+++
@@ -1,8 +0,0 @@
-#' CPAL Mapbox Theme
-#'
-#' Mapbox theme link for CPAL leaflet maps.
-#'
-#' @format ## `cpal_mapbox`
-#' A single attribution row.
-#' @source <mapbox.com>
-"cpal_mapbox"
